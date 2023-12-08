c68ca0e03c3a1dbb7b8d5343676dbdfbf14aae5e
razvan <razzvan.savu@gmail.com>
2023-12-08 11:49:39 +0200

    Validate contact form

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,3 +1,26 @@
-function submit() {
-    alert("Submit");
+function validateAndSubmit() {
+    const title = document.getElementsByName('title')[0].value;
+    const msg = document.getElementsByName('msg')[0].value;
+
+    const titleError = document.getElementById('title-error');
+    const msgError = document.getElementById('msg-error');
+
+    titleError.innerHTML = '';
+    msgError.innerHTML = '';
+
+    let status = true;
+
+    if (!title) {
+        titleError.innerHTML = 'Nu ati introdus un titlu';
+        status = false;
+    }
+
+    if (!msg) {
+        msgError.innerHTML = 'Nu ati introdus un mesaj';
+        status = false;
+    }
+
+    if (status) {
+        document.getElementsByTagName('form')[0].submit();
+    }
 }
