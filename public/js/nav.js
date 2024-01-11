8b847ef8597bfadfd316a521d0c23cc274dfe5ae
razvan <razzvan.savu@gmail.com>
2024-01-11 19:58:57 +0200

    Add hamburger menu functionality

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,3 +1,13 @@
-function clickHamburger() {
-    alert('Click hamburger');
+function openMenu() {
+    document.querySelector('nav').classList.add('drop');
+    document.querySelectorAll('nav > ul > li:has(a)').forEach(e => e.style.display = 'block');
+    document.querySelector('nav > ul > li.open-menu').style.display = 'none';
+    document.querySelector('header > .close-menu').style.display = 'block';
+}
+
+function closeMenu() {
+    document.querySelector('header > .close-menu').style.display = 'none';
+    document.querySelector('nav > ul > li.open-menu').style.display = 'block';
+    document.querySelectorAll('nav > ul > li:has(a)').forEach(e => e.style.display = 'none');
+    document.querySelector('nav').classList.remove('drop');
 }
